193905913b0a6325197107726cb101c47ec6a928
raim95 <raim9595@gmail.com>
2019-08-02 20:26:26 +0300

    fix line 27

--- main.py
+++ main.py
@@ -24,6 +24,6 @@ while True:
     if time_has_changed(prev_update_time):
         prev_update_time = convert_time_to_string(datetime.now())
         client(DeletePhotosRequest(client.get_profile_photos('me')))
-        file = client.upload_file(f"time_images/"+{prev_update_time}+".jpg")
+        file = client.upload_file(f"time_images/"+prev_update_time+".jpg")
         client(UploadProfilePhotoRequest(file))
         time.sleep(50)
